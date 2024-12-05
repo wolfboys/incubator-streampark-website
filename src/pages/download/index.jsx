271e2abc0ccc8385c73f2355ba364b3dc0bd9c7c
benjobs <benjobs@apache.org>
2024-12-05 21:01:43 +0800

    [Improve] Added the verify releases doc (#418)
    * [Improve] update community/security.md
    
    * [Improve] download page added verify releases doc
    
    * Update security.md
    
    * [Improve] i18n improvement

--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -15,12 +15,14 @@ export default function () {
   const archived = downloadDataSource.slice(1, downloadDataSource.length)
   return (
     <Layout>
-      <div className="block download_page container" style={{ padding: "10px 0 30px" }}>
+      <div className="block download_page container" style={{padding: "10px 0 30px"}}>
         <h2 className="fs-4 mb-4 fw-bold">{dataSource.download}</h2>
         <div>
           <span> {dataSource.downloadDesc1}</span>
-          <li>apache-streampark_<span style={{ color: 'red' }}>2.11</span>-<span style={{ color: 'blue' }}>2.1.5</span>-incubating-bin.tar.gz</li>
-          <li>apache-streampark_<span style={{ color: 'red' }}>2.12</span>-<span style={{ color: 'blue' }}>2.1.5</span>-incubating-bin.tar.gz</li>
+          <li>apache-streampark_<span style={{color: 'red'}}>2.11</span>-<span style={{color: 'blue'}}>2.1.5</span>-incubating-bin.tar.gz
+          </li>
+          <li>apache-streampark_<span style={{color: 'red'}}>2.12</span>-<span style={{color: 'blue'}}>2.1.5</span>-incubating-bin.tar.gz
+          </li>
           <span>{dataSource.downloadDesc2}</span>
         </div>
         <h3 className="fs-4 mb-4 mt-4 fw-bold">{dataSource.latestVersion}</h3>
@@ -29,8 +31,8 @@ export default function () {
         <h3 className="fs-4 mb-4 fw-bold">{dataSource.archived}</h3>
         <div className="custom-info-block">
           <div className='d-flex align-items-center'>
-            <InfoSvg className='info-icon' />
-            <p className="custom-block-title">Note</p >
+            <InfoSvg className='info-icon'/>
+            <p className="custom-block-title">Note</p>
           </div>
           <ul>
             <li>{dataSource.note}</li>
@@ -38,16 +40,40 @@ export default function () {
         </div>
         <ReleaseTable dataSource={archived} latest={false}>
         </ReleaseTable>
+        <h3>{dataSource.verifyReleases}</h3>
+        <p>
+          <span>{dataSource.verifyDesc}</span>
+        </p>
+        <p>
+           <span>
+              {dataSource.downloadText} <a
+               href="https://dist.apache.org/repos/dist/dev/incubator/streampark/KEYS">PGP KEYS </a> {dataSource.verifyDesc1} :
+          </span>
+
+          <li> {dataSource.verifyStep1} </li>
+          <pre> gpg --import KEYS </pre>
+
+          <li> {dataSource.verifyStep2} </li>
+          <pre>
+            gpg --verify apache-streampark-***.asc apache-streampark-***.tar.gz
+          </pre>
+
+          <li> {dataSource.verifyStep2} </li>
+          <pre>
+            sha512sum --check apache-streampark-***.sha512
+          </pre>
+        </p>
+
         <h4>License</h4>
         <p>
           <em>
             <span>The software is licensed under the </span>
             <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">
               Apache License 2.0
-            </a>.
-          </em>
-        </p>
-      </div>
+          </a>.
+        </em>
+      </p>
+    </div>
     </Layout>
 
   );
